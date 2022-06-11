e817bc7360ece15eab2d85c51f6edd0cec7544ce
theodorgularte <47986817+thaizagularte@users.noreply.github.com>
2022-06-11 15:17:19 -0300

    função adicionar
    Função para adicionar o produto

--- projeto-estoque/main.py
+++ projeto-estoque/main.py
@@ -1,3 +1,10 @@
 class Ações:
     def __init__(self):
         print("Olá, bem vinde!")
+
+    def adicionarProd(self, prod, max, min):
+        self.prod = prod
+        self.max = max
+        self.min = min
+        print(f"Produto {self.prod} maximo {self.max}")
+        
