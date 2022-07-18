5272cec431e924bdab2cb89e808368825827d186
theodorgularte <47986817+thaizagularte@users.noreply.github.com>
2022-07-18 17:56:56 -0300

    Interface
    Usei o pyqt5, qt desiner para fazer a interface, coisa simples.

--- README.md
+++
@@ -1,2 +0,0 @@
-# projeto-estoque
-Controle de estoque usando python e sqlite.
---
+++ main.py
@@ -0,0 +1,19 @@
+from PySide2.QtWidgets import QTableWidget, QPushButton, QLineEdit, QWidget
+from PySide2.QtWidgets import QApplication, QMainWindow, QFrame
+from ui_pag_principal import Ui_MainWindow
+import sys
+
+class AbrirEstoque(QWidget, Ui_MainWindow):
+    def __init__(self):
+        super(AbrirEstoque, self).__init__()
+        self.setupUi(self)
+        self.setWindowTitle("Estoque mais para mais que para menos")
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    openEst = Ui_MainWindow()
+    janela = QMainWindow()
+    openEst.setupUi(janela)
+    janela.show()
+    sys.exit(app.exec_())
+ 
