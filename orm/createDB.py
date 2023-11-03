b17995b2f9cd4d47cea5dda75ab3cfd84d77c61a
Gularte <47986817+thaizagularte@users.noreply.github.com>
2023-11-03 11:59:04 -0300

    função relatorio

--- orm/createDB.py
+++ orm/createDB.py
@@ -1,4 +1,4 @@
-from sqlalchemy import create_engine, MetaData, Table, Column, Integer, String, Enum
+from sqlalchemy import create_engine, MetaData, Table, Column, Integer, String, Enum, ForeignKey
 
 engine = create_engine('sqlite:///dataBase.db')
 
@@ -10,6 +10,16 @@ itens = Table('itens', metadata,
     Column('nome', String, nullable=False),
     Column('quantidade', Integer, nullable=False),
     Column('categoria', Enum('Eletrônico', 'Decoração', 'Eletrodoméstico', ''), nullable=False)
+)
+
+
+# Modelo da tabela de histórico
+historico = Table('historico', metadata,
+    Column('id', Integer, primary_key=True, autoincrement=True),
+    Column('item_id', Integer, ForeignKey('itens.id'), nullable=False),
+    Column('acao', Enum('Editado', 'Deletado'), nullable=False), 
+    Column('quantidade_ant', String),  
+    Column('quantidade_atual', String)
 )
 
 metadata.create_all(engine)
